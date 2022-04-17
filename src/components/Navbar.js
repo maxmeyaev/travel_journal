4f0fe99deded66874c75eab19b4d217ef8597e1b
maxmeyaev <maxmeyaev05@gmail.com>
2022-04-17 18:04:02 -0400

    Finished navbar
    Need to add underline for login

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,20 @@
-import { ReactComponent as Logo } from '../icons/fill_213.svg'
+import { ReactComponent as Logo2 } from '../icons/T.svg'
 export default function Navbar(){
     return (
-        <header className='navbar d-flex p-20 align-center'>
-            <div className='nav d-flex align-center'>
-                <Logo fill="black"/>
-                <h3 className='d-flex align-center'>Travel Journal</h3>
-                
-            </div>  
-        </header>
+        <div className="wrapper clear">
+            <header className='d-flex align-center justify-between p-20'>
+                <div className='headerLeft d-flex align-center'>
+                    <Logo2 height={150} width={150}/>
+                </div>
+                <ul className='headerRight d-flex align-center'>
+                    <a href="https://developer.mozilla.org/en-US/" className='login'>
+                        <span>Login</span>
+                    </a>
+                    <button className='sign-up-btn'>
+                        <span>Sign Up</span>
+                    </button>
+                </ul>
+            </header>
+        </div>
     )
 }
